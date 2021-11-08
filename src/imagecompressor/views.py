864a0a02dd36b59922236f6e64992501910a746b
Mohamad Daffa Argakoesoemah <argakoesoemahmdaffa@gmail.com>
2021-11-08 20:51:35 +0700

    add image form & display image

--- src/imagecompressor/views.py
+++ src/imagecompressor/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+from .forms import ImageForm
 
 def index(request):
+    image_form = ImageForm()
     context = {
         'heading' : 'Image Compressor',
         'title' : 'Image Compressor',
         'nav' : [
             ['/', 'Beranda'],
             ['/about', 'Tentang Kami'],
-        ]
+        ],
+        'data_form' : image_form,
     }
+
+    if request.method == 'POST':
+        uploaded_image = request.FILES['image']
+        fs = FileSystemStorage()
+        name = fs.save(uploaded_image.name, uploaded_image)
+        print(fs.url(name))
+        context['img_url'] = fs.url(name)
     return render(request, "index.html", context)
